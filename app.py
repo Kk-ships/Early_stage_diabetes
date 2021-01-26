c7eafa310c8fff531fec18a7b8b5d5c94c84acc5
Kaustubh Shirpurkar <50666420+kaustubh-shirpurkar@users.noreply.github.com>
2021-01-26 20:39:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 
+
 def write_prediction(prediction):
     if prediction == 'Positive':
         st.error('You have high probability of having early symptoms for diabetes. Consult a specialist '
@@ -18,27 +19,29 @@ def main():
     partial_paresis = st.radio(label="Have you observed partial loss of voluntary movement?", options=["Yes", "No"])
     Gender = st.radio(label="What is your gender?", options=["Male", "Female"])
     Age = st.slider(label='What is your age?', min_value=10, max_value=100, value=35)
-    df = pd.DataFrame(data = {'Polydipsia': [polydipsia],
-                              'Polyuria':[polyuria],
-                               'sudden weight loss':[weight_loss],
-                                'partial paresis':[partial_paresis],
-                                'Gender':[Gender], 'Age': [Age]})
+    df = pd.DataFrame(data={'Polydipsia': [polydipsia],
+                            'Polyuria': [polyuria],
+                            'sudden weight loss': [weight_loss],
+                            'partial paresis': [partial_paresis],
+                            'Gender': [Gender], 'Age': [Age]})
     if model == 'Random forest':
         pipeline_rf = joblib.load('model_random_forest.sav')
-        write_prediction(pipeline_rf.predict(df)[0])
+        if st.button('Predict'):
+            write_prediction(pipeline_rf.predict(df)[0])
 
     elif model == 'KNN':
         pipeline_knn = joblib.load('model_random_forest.sav')
-        write_prediction(pipeline_knn.predict(df)[0])
+        if st.button('Predict'):
+            write_prediction(pipeline_knn.predict(df)[0])
     elif model == 'SVC':
         pipeline_svc = joblib.load('model_SVC.sav')
-        write_prediction(pipeline_svc.predict(df)[0])
+        if st.button('Predict'):
+            write_prediction(pipeline_svc.predict(df)[0])
 
 
 if __name__ == '__main__':
     st.title('Predict early stage diabetes-mellitus.')
     st.sidebar.markdown('Using a random forest/KNN/SVC classifier on UCI Early stage diabetes risk prediction dataset.')
-
 
     if not st.sidebar.button("About"):
         st.sidebar.text("kaustubh.shirpurkar@gmail.com ")
